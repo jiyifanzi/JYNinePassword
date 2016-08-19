2ed7d697a074409b4beaec5d4fd4913f5676485a
刘东风 <374850759@qq.com>
2016-08-19 19:43:46 +0800

    Initial Commit

---
+++ NinePassWord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NinePassWord
+//
+//  Created by JiYi's on 16/8/19.
+//  Copyright © 2016年 JiYi's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NinePassWord/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NinePassWord
+//
+//  Created by JiYi's on 16/8/19.
+//  Copyright © 2016年 JiYi's. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
